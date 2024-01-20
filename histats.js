26221190e4c6cffb9a0c11a708b396403b18139f
erepinel <156775567+erepinel@users.noreply.github.com>
2024-01-21 02:16:54 +0700

    Delete histats.js

--- histats.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  	let urlParams = new URLSearchParams(window.location.search);
-	let id = urlParams.get('id');
-    let mbot = /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
-    if(id) {
-        if(mbot) location.replace('https://oko.hrosc.com/item/erepinel.github.io/'+id);
-        else location.href = 'https://oko.hrosc.com/item/erepinel.github.io/go/'+id+'/';
-    }
-})();
